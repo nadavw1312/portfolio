96864d273aad5011e6f33b7ea587fdc4f9401393
nadavw1312 <nadavw1312@gmail.com>
2023-09-15 16:12:02 +0300

    - change README

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,17 +4,6 @@ import { SiGithub, SiLinkedin, SiWhatsapp } from "react-icons/si";
 import ConnectWithMe from "../ConnectWithMe";
 
 const Footer = () => {
-  const whatsappUrl =
-    "https://wa.me/972544598264?text=I'm%20interested%20in%20your%20car%20for%20sale";
-
-  const SendEmail = () => {
-    const email = "nadavw1999@gmail.com";
-    const subject = "Job offer";
-    const emailBody = "Hi Nadav, I would like to offer you a job.";
-    document.location =
-      "mailto:" + email + "?subject=" + subject + "&body=" + emailBody;
-  };
-
   return (
     <footer className=" foorer h-28 flex flex-wrap justify-around items-center">
       <div className="footer-text">
@@ -25,7 +14,7 @@ const Footer = () => {
           <br />
           This Website is
           <a
-            href="https://github.com/iamhiman/personal-portfolio-website"
+            href="https://github.com/nadavw1312/portfolio"
             target="_blank"
             className="open-sourced"
           >
